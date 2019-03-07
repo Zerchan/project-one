354aaf535a6a3ef54b83d9ffcb4375c2ee50dd81
Edison Mora <edison.mora@possible.com>
2019-03-06 23:46:59 -0600

    sign up component added

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   //   createDog(parent, args, ctx, info) {
   //     global.dogs = global.dogs || [];
@@ -41,6 +44,29 @@ const Mutations = {
   //   // delete it
   //   return ctx.db.mutation.deleteItem({ where }, info);
   // }
+  async signup(parent, args, ctx, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the DB
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        password,
+        permissions: { set: ['USER'] } // need to use set because permissions is an ENUM
+      }
+    }, info);
+    // create JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // we set the jwt as a cookie on the response
+    ctx.response.cookie('qid', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year
+    })
+    // return the user to the client
+    return user;
+  }
 };
 
 module.exports = Mutations;
